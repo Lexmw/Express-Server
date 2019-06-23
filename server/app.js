b02f19e387e949688fecc41842b57f803ac81ba1
lexius <lexius.waltar@gmail.com>
2019-06-23 13:24:49 -0700

    Create an express server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,27 @@
 // import files and packages up here
+const express = require('express');
+const morgan = require('morgan');
+const data = require('./data.json');
 
+const PORT = 3000;
 
 // create your express server below
-var app;
+const app = express();
 
 // add your routes and middleware below
+
+app.get('/', function(req, res) {
+    res.status(200);
+    res.send('Status Code 200');
+    console.log()
+    done();
+});
+app.get('/data', function(req, res) {
+    res.status(200).send(data);
+    done();
+})
+
+app.use(morgan('dev'));
 
 // finally export the express application
 module.exports = app;
